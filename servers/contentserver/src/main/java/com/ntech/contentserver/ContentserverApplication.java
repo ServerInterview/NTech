e39b11f30879907026329dd859456373809e2c6b
skyhuangdan <skyhuangdan@qq.com>
2020-03-06 20:03:15 +0800

    first

---
+++ servers/contentserver/src/main/java/com/ntech/contentserver/ContentserverApplication.java
@@ -0,0 +1,13 @@
+package com.ntech.contentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentserverApplication.class, args);
+    }
+
+}
---
+++ servers/contentserver/src/test/java/com/ntech/contentserver/ContentserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntech.contentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
