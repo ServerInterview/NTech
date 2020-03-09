914082e6a49386ff68ee85775e6d488a316c92c2
skyhuangdan <skyhuangdan@qq.com>
2020-03-09 21:23:41 +0800

    content add eureka connect

--- servers/contentserver/src/main/java/com/ntech/contentserver/controller/ContentController.java
+++ servers/contentserver/src/main/java/com/ntech/contentserver/controller/ContentController.java
@@ -1,4 +1,22 @@
 package com.ntech.contentserver.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author dan
+ */
+@RestController
 public class ContentController {
+
+
+    @GetMapping("/content1")
+    public String content1(@RequestParam("name") String name){
+        return "hello there " + name;
+    }
+
 }
